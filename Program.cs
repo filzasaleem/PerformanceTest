84fdb40a20efab1ce906a1a202aecca96a1ce149
Filza Saleem <filza.saleem05@gmail.com>
2026-04-14 12:24:52 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapControllers();
+app.Run();
+
+
